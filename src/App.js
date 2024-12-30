d4556cb7ed5de3739c5b6d85457504ba9d48b7b8
abdullah9029 <50084955+abdullah9029@users.noreply.github.com>
2024-12-30 10:44:04 +0500

    Update App.js
    updated list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 // src/App.js  
 import React from 'react';  
 import UserList from './Components/UserList'
-import User from './Components/User';
 import "./index.css"
 
 function App() {  
   return (  
     <div className="App">  
       <UserList />  
-      <User/>
     </div>  
   );  
 }  
